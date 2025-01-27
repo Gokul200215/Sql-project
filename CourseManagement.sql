6398979edfb1091413a170cb986fc0b89050adbc
Gokul200215 <gokulthiru02@gmail.com>
2025-01-27 13:42:26 +0530

    uploaded

--- CourseManagement.sql
+++ CourseManagement.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `student_data_man` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `student_data_man`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: student_data_man
@@ -48,6 +46,283 @@ LOCK TABLES `crs_detail` WRITE;
 INSERT INTO `crs_detail` VALUES (1,'Gokul','Venkadesh','SQl','2024-10-05',15000,5000,'Porur',9876543321,'On Going'),(2,'Krish','Badri','Java','2024-08-15',20000,0,'Porur',8678954604,'Completed'),(3,'Pokesh','Badri','Java','2024-09-11',20000,0,'Vanagaram',8674454644,'Completed'),(4,'kumar','Venkadesh','SQl','2024-07-25',20000,0,'Madhuravoyal',8657754221,'Completed'),(5,'Dinesh','Badri','Java','2024-10-25',6000,14000,'Porur',6678911604,'On Going'),(6,'Selva','Prince','Reactjs','2024-11-11',11000,9000,'Madhuravoyal',8056057974,'Yet to Start'),(7,'Arun','Rakesh','Java Script','2024-11-10',12500,7500,'Vanagaram',8682990307,'Yet to Start'),(8,'Srikanth','Badri','Java','2024-09-03',20000,0,'Madhuravoyal',6383532388,'Completed'),(9,'Manoj','Rakesh','Java Script','2024-08-24',20000,0,'Porur',9884655851,'Completed'),(10,'Senthil','Rakesh','Java Script','2024-10-10',13500,16500,'Vanagaram',9566270443,'On Going'),(11,'Surendhar','Rakesh','Java Script','2024-07-02',20000,0,'Porur',6383352184,'Completed'),(12,'Deepika','Badri','Java','2024-09-22',10000,10000,'Madhuravoyal',6677348302,'On Going'),(13,'karthinika','Venkadesh','SQl','2024-10-20',14000,6000,'Madhuravoyal',6768950231,'On Going'),(14,'Lakshimi','Venkadesh','SQl','2024-10-28',10000,10000,'Porur',7894030211,'On Going'),(15,'Hari','Venkadesh','SQl','2024-07-16',10000,10000,'Valasaravakkam',9886654331,'On Going'),(16,'Sathish','Badri','Java','2024-08-19',20000,0,'Valasaravakkam',9967382133,'Completed'),(17,'Subash','Prince','Reactjs','2024-10-15',16000,4000,'Valasaravakkam',7879664323,'On Going'),(18,'Mothish','Badri','Java','2024-11-08',15000,5000,'Ramapuram',7418939730,'Yet to Start'),(19,'Arul','Prince','Reactjs','2024-11-06',9000,11000,'Valasaravakkam',7675589022,'Yet to Start'),(20,'Priya','Badri','Java','2024-09-27',15500,4500,'Ramapuram',9337678902,'On Going'),(21,'Moni','Prince','Reactjs','2024-10-08',15000,5000,'Ramapuram',8878954334,'On Going'),(22,'karthinika','Venkadesh','SQl','2024-10-20',20000,0,'Madhuravoyal',6768950231,'Completed'),(23,'Ganesh','Badri','Java','2024-11-08',15000,5000,'Ramapuram',8878954334,'Yet to Start');
 /*!40000 ALTER TABLE `crs_detail` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'student_data_man'
+--
+
+--
+-- Dumping routines for database 'student_data_man'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `COURSE` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `COURSE`()
+BEGIN
+SELECT COURSE,TRAINER_NAME FROM CRS_DETAIL GROUP BY COURSE,TRAINER_NAME ORDER BY COURSE;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `CRS_UPDATE` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CRS_UPDATE`(P_PAID_AMT INT,P_BAL_AMT INT,P_STATUS varchar(40),P_STD_ID INT)
+BEGIN
+UPDATE CRS_DETAIL SET PAID_AMT=P_PAID_AMT,BAL_AMT=P_BAL_AMT,STATUS='Completed' WHERE STD_ID=P_STD_ID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `DUE` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DUE`(P_PAID_AMT INT)
+BEGIN
+SELECT * FROM CRS_DETAIL WHERE P_PAID_AMT<=PAID_AMT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `FindByCourse` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FindByCourse`(P_COURSE VARCHAR(30))
+BEGIN
+SELECT * FROM CRS_DETAIL WHERE COURSE=P_COURSE;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `FindByName` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FindByName`(P_STD_NAME varchar(45))
+BEGIN
+select * FROM crs_detail WHERE STD_NAME=P_STD_NAME;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `FindByTrainer` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FindByTrainer`(P_TRAINER_NAME varchar(45))
+BEGIN
+SELECT * FROM CRS_DETAIL WHERE TRAINER_NAME=P_TRAINER_NAME;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `INSERT_DATA` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `INSERT_DATA`(P_STD_NAME varchar(40),P_TRAINER_NAME VARCHAR(40),P_COURSE VARCHAR(30),
+P_JOIN_DATE DATE,P_PAID_AMT INT,P_BAL_AMT INT,P_STD_ADDRESS VARCHAR(50),P_STD_NUM REAL,P_STATUS varchar(40))
+BEGIN
+INSERT INTO crs_detail (STD_NAME,TRAINER_NAME,COURSE,JOIN_DATE,PAID_AMT,BAL_AMT,STD_ADDRESS,STD_NUM,STATUS) VALUES
+(P_STD_NAME,P_TRAINER_NAME,P_COURSE,P_JOIN_DATE,P_PAID_AMT,P_BAL_AMT,P_STD_ADDRESS,P_STD_NUM,P_STATUS);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `NO_DUE` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `NO_DUE`()
+BEGIN
+SELECT * FROM CRS_DETAIL WHERE BAL_AMT=0;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `PAID_ABOVE` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `PAID_ABOVE`(P_PAID_AMT INT)
+BEGIN
+SELECT dense_rank() over(order by PAID_AMT,STD_ID) AS LIST_NO,STD_ID,STD_NAME,TRAINER_NAME,COURSE,JOIN_DATE,PAID_AMT,BAL_AMT,STD_ADDRESS,STD_NUM,STATUS FROM CRS_DETAIL WHERE P_PAID_AMT<PAID_AMT order by PAID_AMT ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `STD_COUNT_IN_COURSE` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `STD_COUNT_IN_COURSE`()
+BEGIN
+SELECT COUNT(COURSE) AS STUDENT_COUNT,COURSE,TRAINER_NAME FROM CRS_DETAIL GROUP BY COURSE,TRAINER_NAME ORDER BY COURSE;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `STUDENT_JOINED_COUNT_IN_MONTH` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `STUDENT_JOINED_COUNT_IN_MONTH`()
+BEGIN
+SELECT DATE_FORMAT(JOIN_DATE,'%M') AS MONTH,COUNT(MONTH(JOIN_DATE)) AS NO_OF_STUDENT_IN_ONE_MONTH FROM 
+CRS_DETAIL GROUP BY DATE_FORMAT(JOIN_DATE,'%M'),MONTH(JOIN_DATE) ORDER BY MONTH(JOIN_DATE);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `UPDATE_AMOUNT` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UPDATE_AMOUNT`(P_STD_ID INT,P_PAID_AMT INT,P_BAL_AMT INT)
+BEGIN
+UPDATE CRS_DETAIL SET PAID_AMT=P_PAID_AMT,BAL_AMT=P_BAL_AMT WHERE STD_ID=P_STD_ID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `UPDATE_ROWS` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UPDATE_ROWS`(P_STD_ID INT,P_STD_NAME varchar(40),P_TRAINER_NAME VARCHAR(40),P_COURSE VARCHAR(30),P_JOIN_DATE DATE,P_PAID_AMT INT,P_BAL_AMT INT,P_STD_ADDRESS VARCHAR(50),P_STD_NUM REAL,P_STATUS varchar(40))
+BEGIN
+UPDATE CRS_DETAIL SET STD_NAME=P_STD_NAME,TRAINER_NAME=P_TRAINER_NAME,COURSE=P_COURSE,JOIN_DATE=P_JOIN_DATE,PAID_AMT=P_PAID_AMT,BAL_AMT=P_BAL_AMT,STD_ADDRESS=P_STD_ADDRESS,STD_NUM=P_STD_NUM,STATUS=P_STATUS WHERE STD_ID=P_STD_ID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `UPDATE_STATUS` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UPDATE_STATUS`(P_STD_ID INT,P_STATUS VARCHAR(50))
+BEGIN
+UPDATE CRS_DETAIL SET STATUS=P_STATUS WHERE STD_ID=P_STD_ID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -58,4 +333,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-14 14:44:16
+-- Dump completed on 2025-01-27 13:36:48
